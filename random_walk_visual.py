6718989bc1c5b4e1f9123d3a91e9cc0cbb3dd449
Jvlia <szylka17@gmail.com>
2023-09-09 22:00:50 +0200

    Update code

--- random_walk_visual.py
+++ random_walk_visual.py
@@ -5,13 +5,25 @@ from random_walk import RandomWalk
 # Tworzenie nowego błądzenia losowego, dopóki program pozostaje aktywny.
 while True:
     # Przygotowanie danych do błądzenia losowego.
-    rw = RandomWalk(5000)
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Wyświetlenie punktów błądzenia losowego.
     plt.style.use('ggplot')
-    fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    fig, ax = plt.subplots(figsize=(10,6), dpi=128)
+
+    # Dodanie, aby intensywność koloru oznaczaa jego kolejność -> efekt gradientu.
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+
+    # Podkreślenie pierwszego i ostatniego punktu błądzenia losowego.
+    ax.scatter(0, 0, c='white', edgecolors='none', s=100)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='blue', edgecolor='none', s=100)
+
+    # Ukrycie osi w celu lepszej wizualizacji tego, co ważne.
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     plt.show()
     keep_running = input("Utworzyć kolejne błądzenie losowe? (t/n): ")
     if keep_running == 'n':
