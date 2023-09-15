398c7b9ed490a916cb77b1ff3eb09a960ff30b9b
Jvlia <szylka17@gmail.com>
2023-09-15 15:23:40 +0200

    Earthquake data visualization

---
+++ eq_explore_data.py
@@ -0,0 +1,10 @@
+import json
+
+# Analiza struktury danych.
+filename = 'data\\eq_data_1_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+readable_file = 'data/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4) # indent=4 nakazuje funkcji dump() sformatowanie danych z wykorzystaniem wcięć dopasowanych do struktury danych.
