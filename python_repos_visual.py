4f07c4975918af7cbc2af0c3f23ca621400ff329
Jvlia <szylka17@gmail.com>
2023-09-20 21:53:51 +0200

    API and github visualization in graphs

--- python_repos_visual.py
+++ python_repos_visual.py
@@ -9,19 +9,25 @@ headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f"Kod stanu: {r.status_code}")
 
-# Umieszczenie odpowiedzi API w zmiennej.
+# Przetworzenie wyników.
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars = [], []
+repo_names, stars, labels = [], [], []
 for repo_dict in repo_dicts:
     repo_names.append(repo_dict['name'])
     stars.append(repo_dict['stargazers_count'])
+
+    owner = repo_dict['owner']['login']
+    description = repo_dict['description']
+    label = f"{owner}<br />{description}" # Ciąg tekstowy HTML znaku nowego wiersza -> <br />
+    labels.append(label)
 
 # Utworzenie wizualizacji
 data = [{
     'type': 'bar',
     'x': repo_names,
     'y': stars,
+    'hovertext': labels,
     'marker': {
         'color': 'rgb(60, 100, 150)',
         'line': {'width': 1.5, 'color': 'rgb(25, 25, 25)'}
